fe285da8e2d1ed63a31f11eab5abb1d467fce2a2
Jero892005 <jero892005@gmail.com>
2026-09-25 13:48:19 -0400

    Added output pdf

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-query = input()
-
-if query == "5 multiplied by 6":
-    answer = 5 * 6
-
-elif query == "8 divided by 3":
-    answer = 8 / 3
-    answer = round(answer, 8)
-
-elif query == "8 divided by 3 without the remainder":
-    answer = 8 // 3
-
-elif query == "8 modulo 3 (remainder only)":
-    answer = 8 % 3
-
-elif query == "4200000 add 23090":
-    answer = 4200000 + 23090
-
-elif query == "4200000 subtract 7633":
-    answer = 4200000 - 7633
-
-else:
-    answer = "Invalid input"
-
+# Multiplication
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+answer = number1 * number2
 print("The answer to your query is:", answer)
-print("Program written by Jeronimo Valencia")
+
+# Division
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+answer = number1 / number2
+print("The answer to your query is:", answer)
+
+# Division without remainder
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+answer = number1 // number2
+print("The answer to your query is:", answer)
+
+# Modulo
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+answer = number1 % number2
+print("The answer to your query is:", answer)
+
+# Addition
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+answer = number1 + number2
+print("The answer to your query is:", answer)
+
+# Subtraction
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+answer = number1 - number2
+print("The answer to your query is:", answer)
+
+print("Jeronimo Valencia")
